2394bdc7d138c4edb2dfe33839bf5edc07eb3176
MonikaJaz <145592148+MonikaJaz@users.noreply.github.com>
2023-11-14 21:27:06 +0100

    final quiz

--- QuizGame.py
+++ QuizGame.py
@@ -5,11 +5,36 @@ playing = input("Do you want ot play? ")
 if playing.lower() != "yes":
     quit()
 print("Okay let's play :)")
+score = 0
 
 answer = input("What does CPU stand for? ")
-if answer == "central processing unit":
+if answer.lower() == "central processing unit":
     print("Correct!")
+    score += 1
 else:
     print("Wrong answer!")
+answer = input("What does GPU stand for? ")
+if answer.lower() == "graphics processing unit":
+    print("Correct!")
+    score += 1
+else:
+    print("Wrong answer!")
+answer = input("What does RAM stand for? ")
+if answer.lower() == "random access memory":
+    print("Correct!")
+    score += 1
+else:
+    print("Wrong answer!")
+answer = input("What does PSU stand for? ")
+if answer.lower() == "power supply":
+    print("Correct!")
+    score += 1
+else:
+    print("Wrong answer!")
+print("You got" + str(score) + "questions correct!")
+print("You got " + str(score/4 * 100) + "%")
+
+
+
 
 
